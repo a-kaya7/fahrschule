83597075af9311f864cd16aceae9d5426874ca1b
ahmet <ahmet@gmail.com>
2025-05-10 01:22:15 +0200

    develop

---
+++ Fahrschule/src/test/java/net/fahrschule/FahrschuleApplicationTests.java
@@ -0,0 +1,13 @@
+package net.fahrschule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FahrschuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
